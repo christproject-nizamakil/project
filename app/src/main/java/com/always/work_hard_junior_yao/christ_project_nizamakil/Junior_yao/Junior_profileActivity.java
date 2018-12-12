ce389b892637bc7c1008667997a72d25f4bd9d81
Junior_Yao <junioryao.jy@gmail.com>
2018-12-12 10:28:42 +0530

    profile activity

---
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/Junior_yao/Junior_profileActivity.java
@@ -0,0 +1,13 @@
+package com.always.work_hard_junior_yao.christ_project_nizamakil.Junior_yao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Junior_profileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_junior_profile);
+    }
+}
