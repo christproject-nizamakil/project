c035459a61b5591a32e1370f4fc4389e0df81d3f
amir <bokhitamirhar@gmail.com>
2018-12-12 10:32:52 +0530

    this is a test 1

---
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/Freeman/Freeman_Home.java
@@ -0,0 +1,13 @@
+package com.always.work_hard_junior_yao.christ_project_nizamakil.Freeman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Freeman_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freeman__home);
+    }
+}
