1c7587c92ddfd77d60e741a4116d702e19696ff6
JuniasOnesime <45413000+JuniasOnesime@users.noreply.github.com>
2018-12-19 09:27:05 +0530

    junias'new work

--- app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/Junias/fragment_order.java
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/Junias/fragment_order.java
@@ -1,6 +1,8 @@
 package com.always.work_hard_junior_yao.christ_project_nizamakil.Junias;
 
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 
 public class fragment_order extends Fragment {
+    private Button button1;
 }
