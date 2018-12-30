02128c5159d40a03aca42e502fc98067d629eb89
JuniasOnesime <45413000+juniasonesime@users.noreply.github.com>
2018-12-31 02:27:32 +0530

    junias_fragment

--- app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/Junias/fragment_order.java
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/Junias/fragment_order.java
@@ -1,8 +1,20 @@
 package com.always.work_hard_junior_yao.christ_project_nizamakil.Junias;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class fragment_order extends Fragment {
-    private Button button1;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_order, container, false);
+    }
 }
