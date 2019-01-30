7877a71740b9fafcdeba12fca83e5f7d5e8ce0f1
Junior_Yao <junioryao.jy@gmail.com>
2019-01-30 15:02:16 +0530

    profile

---
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/login_registration_pack/Register_acti.java
@@ -0,0 +1,13 @@
+package com.always.work_hard_junior_yao.christ_project_nizamakil.login_registration_pack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register_acti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_acti);
+    }
+}
