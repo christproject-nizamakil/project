2dc38120a6b5498746d10f2bca111ccf1ed5cee9
JuniasOnesime <45413000+juniasonesime@users.noreply.github.com>
2019-02-16 14:29:43 +0530

    junias'new work

--- app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/cartAdapter.java
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/cartAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 public class cartAdapter extends RecyclerView.Adapter<cartAdapter.cartViewHolder> {
     private ArrayList<cart_items> mCartList;
 
-    public static class cartViewHolder extends  RecyclerView.ViewHolder {
+    public static class cartViewHolder extends  RecyclerView.ViewHolder{
 
         public ImageView image;
         public TextView mTextView11;
@@ -26,66 +27,76 @@ public class cartAdapter extends RecyclerView.Adapter<cartAdapter.cartViewHolder
         public int mvalues;
 
 
-        public cartViewHolder(@NonNull View itemView) {
+
+        public cartViewHolder(@NonNull final View itemView) {
             super(itemView);
 
-            image = itemView.findViewById(R.id.imageView1);
-            mTextView11 = itemView.findViewById(R.id.textView2);
-            mTextView22 = itemView.findViewById(R.id.textView3);
-            mTextView23 = itemView.findViewById(R.id.textView4);
-            button1 = itemView.findViewById(R.id.button);
-            button2 = itemView.findViewById(R.id.button1);
+            image=itemView.findViewById(R.id.imageView1);
+            mTextView11=itemView.findViewById(R.id.textView2);
+            mTextView22= itemView.findViewById(R.id.textView3);
+            mTextView23= itemView.findViewById(R.id.carttextview);
+            button1= itemView.findViewById(R.id.button);
+            button2= itemView.findViewById(R.id.button1);
             button1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    try {
+                    String w = mTextView23.getText().toString().trim();
+                    mvalues=Integer.parseInt(w);
+                    mvalues++;
+                    mTextView23.setText(Integer.toString(mvalues));
+                   // button2.setVisibility(View.INVISIBLE);
 
-                        mvalues = Integer.parseInt(mTextView23.toString());
-                        for (int i=0; i< 100; i++)
-                        {
-                            mvalues = i;
-                            i++;
-                        }
+                }
+            });
+
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
 
-                    } catch (Exception e) {
+                    String w = mTextView23.getText().toString().trim();
+                    mvalues=Integer.parseInt(w);
+                    mvalues= mvalues-1;
+                    mTextView23.setText(Integer.toString(mvalues));
+
+                    if (mvalues==0)
+                    {
 
                     }
+
                 }
-
-
             });
+
         }
     }
 
-
-        public cartAdapter(ArrayList<cart_items> CartList) {
-            mCartList = CartList;
-        }
-
-        @NonNull
-        @Override
-        public cartViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cart_items, viewGroup, false);
-            cartViewHolder junias1 = new cartViewHolder(v);
-            return junias1;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull cartViewHolder cartViewHolder, int i) {
-
-
-            cart_items currentItem = mCartList.get(i);
-            cartViewHolder.image.setImageResource(currentItem.getImageResources());
-            cartViewHolder.mTextView11.setText(currentItem.getText1());
-            cartViewHolder.mTextView22.setText(currentItem.getText2());
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mCartList.size();
-        }
+    public cartAdapter(ArrayList<cart_items> CartList)
+    {
+        mCartList = CartList;
     }
+
+    @NonNull
+    @Override
+    public cartViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cart_items,viewGroup,false);
+        cartViewHolder junias1 = new cartViewHolder(v);
+        return  junias1;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull cartViewHolder cartViewHolder, int i) {
+
+
+        cart_items currentItem = mCartList.get(i);
+        cartViewHolder.image.setImageResource(currentItem.getImageResources());
+        cartViewHolder.mTextView11.setText(currentItem.getText1());
+        cartViewHolder.mTextView22.setText(currentItem.getText2());
+
+    }
+
+    @Override
+    public int getItemCount() { return mCartList.size(); }
+}
+
 
--- app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/fragment_amir_cart.java
+++ app/src/main/java/com/always/work_hard_junior_yao/christ_project_nizamakil/fragment_amir_cart.java
@@ -7,8 +7,11 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,7 +24,7 @@ public class fragment_amir_cart extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root= inflater.inflate(R.layout.amir_fragment_card,container,false);
+        final View root= inflater.inflate(R.layout.amir_fragment_card,container,false);
 
 
 
@@ -45,7 +48,24 @@ public class fragment_amir_cart extends Fragment {
         adapter1= new cartAdapter(CartList);
 
 
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView recyclerView, @NonNull MotionEvent motionEvent) {
+                Toast.makeText(getContext(), "click", Toast.LENGTH_SHORT).show();
 
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView recyclerView, @NonNull MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean b) {
+
+            }
+        });
 
         recyclerView.setLayoutManager(layoutManager1);
         recyclerView.setAdapter(adapter1);
